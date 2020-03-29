b7f08d2bc9304d46d83dcab51c711187608121ee
Mohammed <github email address>
2020-03-29 13:21:39 +0300

    Updates

---
+++ precache-manifest.14a4f785b5e2753a2b53775085b741c9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08129161dd56293c65b9048e545d0ac9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c84b6d2f9c114e7fc0de",
+    "url": "/portfolio/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "50dd27597b3327280083",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c84b6d2f9c114e7fc0de",
+    "url": "/portfolio/static/js/2.178c8789.chunk.js"
+  },
+  {
+    "revision": "7c825aad4b2ed3c4240e10f596e730bb",
+    "url": "/portfolio/static/js/2.178c8789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50dd27597b3327280083",
+    "url": "/portfolio/static/js/main.c6cfc727.chunk.js"
+  },
+  {
+    "revision": "7edd33000800508e9898",
+    "url": "/portfolio/static/js/runtime-main.130d373a.js"
+  },
+  {
+    "revision": "e6892988bdd700e4a702dc10cdf28dd2",
+    "url": "/portfolio/static/media/Mohammed-Abdul-Barry.e6892988.png"
+  },
+  {
+    "revision": "974cc3d99e842b1af9b5914d38cb9ef3",
+    "url": "/portfolio/static/media/afis-1.974cc3d9.png"
+  },
+  {
+    "revision": "93806f7c695cc4b752293fa6b688795f",
+    "url": "/portfolio/static/media/bg.93806f7c.jpg"
+  },
+  {
+    "revision": "b46a8094b349d9d0fd5b17412159d53b",
+    "url": "/portfolio/static/media/mentorship.b46a8094.png"
+  },
+  {
+    "revision": "b88ed19fdabdcecd4bb9f41309c8f4aa",
+    "url": "/portfolio/static/media/mines-weeper.b88ed19f.png"
+  }
+]);
