8f3dc95674b8e370968f1ee6afd1294cdb66d46a
MohammedAlabd <mu96abd@gmail.com>
2020-06-03 13:38:17 +0300

    Updates

---
+++ precache-manifest.b89509d8cf34263b20c315c2a5a810d9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd61137d51cec5ce2eb8de953b92f0cf",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c84b6d2f9c114e7fc0de",
+    "url": "/portfolio/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "e388a9de8c2b5f30488d",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c84b6d2f9c114e7fc0de",
+    "url": "/portfolio/static/js/2.178c8789.chunk.js"
+  },
+  {
+    "revision": "7c825aad4b2ed3c4240e10f596e730bb",
+    "url": "/portfolio/static/js/2.178c8789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e388a9de8c2b5f30488d",
+    "url": "/portfolio/static/js/main.2187c158.chunk.js"
+  },
+  {
+    "revision": "7edd33000800508e9898",
+    "url": "/portfolio/static/js/runtime-main.130d373a.js"
+  },
+  {
+    "revision": "e6892988bdd700e4a702dc10cdf28dd2",
+    "url": "/portfolio/static/media/Mohammed-Abdul-Barry.e6892988.png"
+  },
+  {
+    "revision": "974cc3d99e842b1af9b5914d38cb9ef3",
+    "url": "/portfolio/static/media/afis-1.974cc3d9.png"
+  },
+  {
+    "revision": "2c9f6ee5ac6fbedb8147715cf4f19dae",
+    "url": "/portfolio/static/media/bg.2c9f6ee5.jpg"
+  },
+  {
+    "revision": "b46a8094b349d9d0fd5b17412159d53b",
+    "url": "/portfolio/static/media/mentorship.b46a8094.png"
+  },
+  {
+    "revision": "b88ed19fdabdcecd4bb9f41309c8f4aa",
+    "url": "/portfolio/static/media/mines-weeper.b88ed19f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.88a48e5e83c41c32131d272e6bd8ba77.js"
+  "/portfolio/precache-manifest.b89509d8cf34263b20c315c2a5a810d9.js"
 );
 
 self.addEventListener('message', (event) => {
